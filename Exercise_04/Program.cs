1cc77521769190491e5897e292b6feda8193694b
Curly_Kayl <endwarfor@gmail.com>
2023-01-16 13:15:51 +0300

    Exercise_04 during

--- Exercise_04/Program.cs
+++ Exercise_04/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.WriteLine("Введите первое число: ");
+string numberStr1 = Console.ReadLine();
+int number1 = Convert.ToInt32(numberStr1);
+
+
+Console.WriteLine("Введите второе число: ");
+string numberStr2 = Console.ReadLine();
+int number2 = Convert.ToInt32(numberStr2);
+
+Console.WriteLine("Введите третье число: ");
+string numberStr3 = Console.ReadLine();
+int number3 = Convert.ToInt32(numberStr3);
+
+if ()
