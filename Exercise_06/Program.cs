4065bdc73cd18bb328fdc021931d3ee6112bae03
Curly_Kayl <endwarfor@gmail.com>
2023-01-16 20:45:15 +0300

    Exercise_06 complete

--- Exercise_06/Program.cs
+++ Exercise_06/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.WriteLine("Введите число: ");
+string numberStr = Console.ReadLine();
+int number = Convert.ToInt32(numberStr);
+int result = number % 2;
+
+if (result == 0)
+{
+	Console.WriteLine("Число является четным.");
+}
+else
+{
+	Console.WriteLine("Число является нечетным.");
+}
