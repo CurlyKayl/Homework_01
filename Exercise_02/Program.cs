02de01309831f577451ede71a1ebaeef79d7e65b
Curly_Kayl <endwarfor@gmail.com>
2023-01-16 12:48:21 +0300

    Exercise_02 complete

--- Exercise_02/Program.cs
+++ Exercise_02/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.WriteLine("Введите первое число: ");
+string numberStr1 = Console.ReadLine();
+int number1 = Convert.ToInt32(numberStr1);
+
+
+Console.WriteLine("Введите второе число: ");
+string numberStr2 = Console.ReadLine();
+int number2 = Convert.ToInt32(numberStr2);
+
+if (number1 > number2)
+{
+	Console.WriteLine($"Число {number1} больше, чем число {number2}.");
+}
+else
+{
+	Console.WriteLine($"Число {number2} больше, чем число {number1}.");
+}
